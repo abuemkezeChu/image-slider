f9bf6a2843ed89ab24adb89b3c5fc0a320fb41f7
Abuemkeze Chu <abuemkeze@gmail.com>
2023-08-24 00:28:21 +0100

    feat: implement image slider

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,1 +1,32 @@
 'use strict'
+
+let index = 0
+let images = []
+let time = 3000
+
+images[0] = 'assets/images/apples.jpg'
+images[1] = 'assets/images/duck.jpg'
+images[2] = 'assets/images/food.jpg'
+images[3] = 'assets/images/honey.jpg'
+images[4] = 'assets/images/ice-cream.jpg'
+images[5] = 'assets/images/pancakes.jpg'
+images[6] = 'assets/images/pizza.jpg'
+images[7] = 'assets/images/pizza2.jpg'
+images[8] = 'assets/images/spaghetti.jpg'
+images[9] = 'assets/images/table.jpg'
+console.log(images);
+
+const changeImage = () => {
+  let imgView = document.getElementById('slide')
+  let imgElement = imgView.querySelector('img')
+  imgElement.src = images[index]
+  if (index < images.length - 1) {
+    index++
+  } else {
+    index = 0
+  }
+
+  setTimeout('changeImage()', time)
+}
+
+window.onload = changeImage;
